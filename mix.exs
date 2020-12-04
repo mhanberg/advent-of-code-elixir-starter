9c769839454dfad123b5ed5e742d2bcbd849cb7a
Daniel Flanagan <daniel@lyte.dev>
2020-12-03 18:25:32 -0600

    Update mix.exs
    Co-authored-by: Mitchell Hanberg <mitch@mitchellhanberg.com>

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AdventOfCode.MixProject do
     [
       app: :advent_of_code,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
