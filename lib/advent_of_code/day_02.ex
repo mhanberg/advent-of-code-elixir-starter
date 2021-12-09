0ec8820c337bd8e3451ad92baa783f96e84a429c
Lorena <lorena@bunsan.io>
2021-12-09 01:53:42 -0600

    Solve part 1 using (horizontal, depth) coordinates

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,71 @@
 defmodule AdventOfCode.Day02 do
+  @doc """
+  It takes a list of coordinates with the format {x, y} and defining {0, 0} as the initial coordinate,
+  increments the x and y values using those coordinates. At the end returns the multiply x*y.
+
+    - x represents the horizontal value and
+    - y represents a depth value.
+
+  ## Examples
+
+      iex> AdventOfCode.Day02.part1([{1, 0}, {0, 5}, {0, -3}])
+      > x = x + 1 + 0 + 0
+      > y = y + 0 + 5 - 3
+      > {1, 2}
+      > 1 * 2
+      iex> 2
+  """
   def part1(args) do
+    start_coordinate = {0, 0}
+
+    {final_horizontal, final_depth} =
+      args
+      |> define_coordinates()
+      |> Enum.reduce(start_coordinate, fn {x, y}, {horizontal, depth} ->
+        {horizontal + x, depth + y}
+      end)
+
+    final_horizontal * final_depth
+  end
+
+  @doc """
+  It takes a list of instructions and convert each one into a coordinate representation following the rules:
+    - forward N increases the horizontal position by N units.
+    - down N increases the depth by N units.
+    - up N decreases the depth by N units.
+
+  Considering the coordinates (x, y) the forward instruction modified the x value (horizontal) and the other
+  ones modify the y value (depth).
+
+  The coordinate that is not modified is represented by 0.
+
+  ## Examples
+
+      iex> AdventOfCode.Day02.define_coordinates(["forward 1", "down 5", "up 3"])
+      iex> [{1, 0}, {0, 5}, {0, -3}]
+  """
+  @spec define_coordinates(list()) :: [tuple()]
+  def define_coordinates(input) do
+    Enum.map(input, fn instruction -> coordinate(instruction) end)
+  end
+
+  defp coordinate(instruction) do
+    case instruction do
+      "forward " <> value ->
+        x = String.to_integer(value)
+        {String.to_integer(value), 0}
+
+      "down " <> value ->
+        y = String.to_integer(value)
+        {0, y}
+
+      "up " <> value ->
+        y = String.to_integer(value)
+        {0, -y}
+
+      _ ->
+        {0, 0}
+    end
   end
 
   def part2(args) do
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,19 +3,49 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  describe "part1" do
+    # Start coordinate = (0, 0)
+    test "Define a tuple for forward instruction: horizontal" do
+      input = ["forward 5"]
+      result = define_coordinates(input)
 
-    assert result
+      assert result = [{5, 0}]
+    end
+
+    test "Define a tuple for down instruction: depth (+)" do
+      input = ["down 5"]
+      result = define_coordinates(input)
+
+      assert result = [{0, 5}]
+    end
+
+    test "Define a tuple for up instruction: depth (-)" do
+      input = ["up 5"]
+      result = define_coordinates(input)
+
+      assert result = [{0, -5}]
+    end
+
+    test "Creates the list of values for horizontal and depth positions" do
+      input = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
+      result = define_coordinates(input)
+
+      assert result = [{5, 0}, {0, 5}, {8, 0}, {0, -3}, {0, 8}, {2, 0}]
+    end
+
+    test "It takes a list of coordinates and define the horizontal and depth positions" do
+      input = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
+      result = part1(input)
+
+      assert result = 15
+    end
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  # @tag :skip
+  # test "part2" do
+  #   input = nil
+  #   result = part2(input)
 
-    assert result
-  end
+  #   assert result
+  # end
 end
